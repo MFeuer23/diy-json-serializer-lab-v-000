1b53af96abfe518a97a793e4fba2c7ab5bc5f332
Mallory Feuer <mallory.feuer@gmail.com>
2018-11-09 17:41:06 +0000

    automatically backed up by learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,7 +5,7 @@ class ProductsController < ApplicationController
   
   def data
     product = Product.find(params[:id])
-    render json: ProductSerializer
+    render json: ProductSerializer.serialize(product)
   end
 
   def inventory
